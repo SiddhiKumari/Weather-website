84c4d9b09fe4e22d68e1eca76770f34c334b699d
SiddhiKumari <97723826+SiddhiKumari@users.noreply.github.com>
2022-12-29 18:10:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const options = {
+const options = {   //options is an object which mentions which request is it- "GET"(fetch)or"POST"(upload)
   method: "GET",
 };
-const getWeather = (city) => {
-  cityname.innerHTML = city;
+//API return results in form of object containing all weather reports
+const getWeather = (city) => {    //get weather is an arrow function with parameter  city
+  cityname.innerHTML = city;  //cityname = id, in which name of city is stored
 
-  // fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city,options)
-  fetch(
-    "http://api.weatherapi.com/v1/current.json?key=ac85b9b5cda34b92a7c75801222512&q=" +
-      city,
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response);
+  fetch("http://api.weatherapi.com/v1/current.json?key=ac85b9b5cda34b92a7c75801222512&q=" +city,options)  //fetch function generatibg response from api url
+    .then((response) => response.json())  // response is an arrow function converting to json format
+    .then((response) => {  // anotherarrow function
+      console.log(response);  //prints response in console section
       
+      //temp_c2=id in html where we store the response
+      //response is in form of object which contains another object=current inside which we have the data.
       temp_c2.innerHTML = response.current.temp_c;
       temp_c.innerHTML = response.current.temp_c;
       temp_f.innerHTML = response.current.temp_f;
@@ -29,15 +27,13 @@ const getWeather = (city) => {
       wind_kph.innerHTML = response.current.wind_kph;
       wind_degree.innerHTML = response.current.wind_degree;
       wind_dir.innerHTML = response.current.wind_dir;
-      
-      
     })
-    .catch((err) => console.error(err));
+    .catch((err) => console.error(err));  //if any error then catch
 };
 
-submit.addEventListener("click", (e) => {
-  e.preventDefault();
-  getWeather(city.value);
+submit.addEventListener("click", (e) => {  //submit =id of button
+  e.preventDefault();  //prevents default reload
+  getWeather(city.value); //when submit clicked check weather for a particular city
 });
 
-getWeather("Delhi");
+getWeather("Delhi");  //by default called for delhi
